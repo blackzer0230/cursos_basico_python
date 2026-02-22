7c063e0529d1dc227ca86bd389cdbdd2d937e792
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-22 13:47:59 -0400

    todavia con los set

--- 02_listas/02_sets.py
+++ 02_listas/02_sets.py
@@ -7,6 +7,35 @@ print(type(mi_otro_set)) #inicialmente dice que es diccionario
 
 
 mi_otro_set = {"choso", "gonlez", 20}
-print(type(mi_otro_set))
+print(type(mi_otro_set)) #ahora cambia a set
+
+print(len(mi_otro_set))
 
 
+mi_otro_set.add("sthefanny")
+
+print(mi_otro_set) # un set o es una estructura ordenada
+
+
+mi_otro_set.add("choso") # un set no admite repetidos 
+print(mi_otro_set)
+
+# para comprobar asi un elemento existe en un set 
+print("choso" in mi_otro_set)
+print("chosi" in mi_otro_set)
+
+
+mi_otro_set.remove("gonlez")
+print(mi_otro_set)
+
+
+mi_otro_set.clear()
+print(mi_otro_set)
+print(len(mi_otro_set))
+
+
+
+
+
+
+# ventajas del set?

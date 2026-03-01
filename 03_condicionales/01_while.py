fccb7ca164983956ead834fb4a99beec5b1261c4
blackzer0230 <harry.gonlez0230@gmail.com>
2026-03-01 14:00:30 -0400

    apuntes de whiles

---
+++ 03_condicionales/01_while.py
@@ -0,0 +1,42 @@
+    ### bucles ###
+
+# while
+
+mi_condicion = 0
+
+while mi_condicion < 10: # se  ejecuta si la condicion es verdadera en este caso la condicion es menor? verdadero  
+    print(mi_condicion)
+    mi_condicion += 2
+else: # es opcional 
+    print("mi condicion ya es mayor o igual a 10")
+
+
+
+
+otra_condicion = 0 
+
+while otra_condicion < 20:
+    otra_condicion +=2
+    if otra_condicion == 16:
+        print("se detiene la ejecucion")
+        break   # se detiene la condicion si se cumple cierta condicion 
+
+    print(f"mi condicion es menor que 20 y va por {otra_condicion}")
+
+
+# FOR 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
